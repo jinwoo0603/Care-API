f50bb99d2466c72587e0f59d1af2549819ccad56
jinwoo baek <jinwoo060360@gmail.com>
2025-04-30 17:23:11 +0900

    added some utility enum

--- app/models/utils.py
+++ app/models/utils.py
@@ -4,3 +4,8 @@ class MEASUREMENT_TYPE(Enum):
     BLOOD_SUGAR = "blood_sugar"
     BLOOD_PRESSURE = "blood_pressure"
     WEIGHT = "weight"
+
+class RESULT_CODE(Enum):
+    SUCCESS = 1
+    NOT_FOUND = -2
+    FAILED = -3
